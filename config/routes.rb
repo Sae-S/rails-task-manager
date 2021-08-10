251aaff7beb0051d79f66c77961ed66318012534
Sae Shikiji <saeshikiji@gmail.com>
2021-08-10 16:26:29 +0900

    Added checkboxes and can not update the completed status

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   get 'tasks/new', to: 'tasks#new', as: :new_task
   post '/tasks', to: 'tasks#create'
   get 'tasks/:id', to: 'tasks#show', as: :task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  delete 'tasks/:id', to: 'tasks#destroy'
+  patch 'tasks/:id', to: 'tasks#update'
+
 end
